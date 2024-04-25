e804f9a019651cbb22347b2f73e8eb36cf591c05
ejcleary <ejcleary03@gmail.com>
2024-04-25 14:13:28 -0400

    adding csv files

--- sort_methods.cpp
+++ sort_methods.cpp
@@ -12,42 +12,37 @@ void swap(int *a, int *b) {
     *b = t;
 }
 
-int partition(int array[], int low, int high)
-{
+int partition(int array[], int low, int high) {
     // select the rightmost element as pivot
     int pivot = array[low];
     int up=low, down=high;
 
     // while up is less than down
-    while(up<down)
-    {
+    while(up<down){
         // move up continuously until you find an element greater than pivot
-        for (int j = up; j < high; j++)
-        {
+        for (int j = up; j < high; j++){
             if(array[up]>pivot)
                 break;
             up++;
         }
         // move down continuously until you find an element less than pivot
-        for (int j = high; j > low; j--)
-        {
+        for (int j = high; j > low; j--){
             if(array[down]<pivot)
                 break;
             down--;
         }
         // if up is less than down, swap the elements at up and down
-        if(up<down)
+        if(up<down) {
             swap(&array[up], &array[down]);
+        }
     }
     // swap pivot with the element at down
     swap(&array[low], &array[down]);
     return down;
 }
 
-void quickSort(int array[], int low, int high)
-{
-    if (low < high)
-    {
+void quickSort(int array[], int low, int high) {
+    if (low < high){
         // find pivot element such that element smaller than pivot are on the left and greater on the right
         int pivot = partition(array, low, high);
         // recursively call quicksort on the two subarrays
@@ -65,40 +60,37 @@ void merge(int arr[], int left, int mid, int right) {
     int X[n1], Y[n2];
 
     // copy data to X and Y
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++) {
         X[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
+    }
+    for (int j = 0; j < n2; j++) {
         Y[j] = arr[mid + 1 + j];
+    }
 
     // merge the two temporary arrays back into the original array
     int i, j, k;
     i = 0, j = 0, k = left;
 
     // while elements in X and Y remain, compare the elements of X and Y
-    while (i < n1 && j < n2)
-    {
-        if (X[i] <= Y[j])
-        {
+    while (i < n1 && j < n2){
+        if (X[i] <= Y[j]){
             arr[k] = X[i];
             i++;
         }
-        else
-        {
+        else{
             arr[k] = Y[j];
             j++;
         }
         k++;
     }
     // When we run out of elements in either X or Y append the remaining elements
-    while (i < n1)
-    {
+    while (i < n1){
         arr[k] = X[i];
         i++;
         k++;
     }
 
-    while (j < n2)
-    {
+    while (j < n2){
         arr[k] = Y[j];
         j++;
         k++;
@@ -106,8 +98,7 @@ void merge(int arr[], int left, int mid, int right) {
 }
 
 void mergeSort(int arr[], int left, int right) {
-    if (left < right)
-    {
+    if (left < right){
         // m is the point where the array is divided into two subarrays
         int mid = left + (right - left) / 2;
 
