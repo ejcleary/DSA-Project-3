8d90dfe7225bbacf1a3e02e2b9073589b8ae6f9e
jacs <128555084+yakubmungai@users.noreply.github.com>
2024-04-25 20:39:01 +0000

    finished races and drivers csv parsing

--- driverdatacsv.cpp
+++ driverdatacsv.cpp
@@ -5,20 +5,47 @@
 
 using namespace std;
 
-void readDriverFile {
-unordered_map<int,tuple<string, string, int> driverData>
-//Inspired by geeksforgeeks
+void readDriverFile() {  //reads the contents of the driver files and returns
+unordered_map<int,tuple<string, string, int> driverData> driversMap;
+
+//Code to read file inspired by geeksforgeeks
 
 ifstream driversFile("drivers.csv");
 string line;
 
 while (getline(driversFile, line)) {
     stringstream ss(line);
-    //Process data and store in map
+    string driverID, firstName, surName, number;
+    getline(ss, driverID, ',');
+    getline(ss, firstName, ',');
+    getline(ss, surName, ',');
+    getline(ss, number, ',');
+    driversMap[driverID] = make_tuple(firstName, surName, number);
+}
+driversFile.close()
 }
 
 
-driversFile.close()
+
+void readRacesFile() {  //reads the contents of the driver files and returns
+unordered_map<int,tuple<string, string, int> driverData> racesMap;
+
+
+//Code to read file inspired by geeksforgeeks
+
+ifstream racersFile("racers.csv");
+string line;
+
+while (getline(racersFile, line)) {
+    stringstream ss(line);
+    string id, year, round, name;
+    getline(ss, id, ',');
+    getline(ss, year, ',');
+    getline(ss, round, ',');
+    getline(ss, name, ',');
+    drivers[id] = make_tuple(year, round, name);
+}
+racersFile.close()
 }
 
 
